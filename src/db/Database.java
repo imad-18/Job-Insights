ac5f7597b38b2f2817180e2b87466cb5fba3d17f
abdelhamid <abdelhamid.hachimi.alaoui@gmail.com>
2024-12-28 00:24:10 +0100

    Connection front vr2

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,127 +1,157 @@
 package db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.ArrayList;
-
+import java.util.List;
 import model.Annonce;
 
 public class Database {
 
-	// Database connection details
-	private String url = "jdbc:mysql://localhost:3306/javaproject";
-	private String user = "root";
-	private String password = "";
+	private Connection connection;
 
-	public void insertData(ArrayList<Annonce> listAnnonce) {
-		// JDBC objects
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-
-		// la requeette d insertion
-		String insertQuery = "INSERT INTO annonce (title, location, description, StartDate, EndDate, PostsNum, Secteur, Fonction, Experience, EtudeLevel, "
-				+ "ContratDetails) VALUES (?, ?, ?, STR_TO_DATE(REGEXP_REPLACE(?, '[.]', '/'), '%d/%m/%Y'), STR_TO_DATE(REGEXP_REPLACE(?, '[.]', '/'), '%d/%m/%Y'),"
-				+ " ?, ?, ?, ?, ?, ?)";
+	// Constructor to initialize the database connection
+	public Database(String dbUrl, String username, String password) throws SQLException {
 		try {
-			// Loading the connection
-			Class.forName("com.mysql.cj.jdbc.Driver");
-
-			// creation de la connexion avec url mdp et username
-			connection = DriverManager.getConnection(url, user, password);
-			System.out.println("Connected to the database!");
-
-		} catch (Exception e) {
-			e.printStackTrace();
+			connection = DriverManager.getConnection(dbUrl, username, password);
+		} catch (SQLException e) {
+			throw new SQLException("Failed to connect to the database: " + e.getMessage());
 		}
-
-		ArrayList test = this.selectData();
-		if(test.size() == 0) {
-			int i = listAnnonce.size();
-			for(Annonce item : listAnnonce) {
-				try {
-					preparedStatement = connection.prepareStatement(insertQuery);
-
-					preparedStatement.setString(1, item.getTitle());
-					preparedStatement.setString(2, item.getLocation());
-					preparedStatement.setString(3, item.getDescription());
-					preparedStatement.setString(4, item.getStartDate());
-					preparedStatement.setString(5, item.getEndDate());
-					preparedStatement.setInt(6, item.getPostsNum());
-					preparedStatement.setString(7, item.getSecteur());
-					preparedStatement.setString(8, item.getFonction());
-					preparedStatement.setString(9, item.getExperience());
-					preparedStatement.setString(10, item.getEtudeLevel());
-					preparedStatement.setString(11, item.getContracDetails());
-
-					// Step 4: Execute the query
-					int rowsInserted = preparedStatement.executeUpdate();
-					if (rowsInserted > i) {
-						System.out.println("A new row ("+i+") was inserted successfully!");
-						i++;
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		else {
-			System.out.println("rows already added ...");
-		}
-		// Step 5: Close the resources
-		try {
-			if (preparedStatement != null) preparedStatement.close();
-			if (connection != null) connection.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-
 	}
 
-	public ArrayList<Annonce> selectData() {
-		ArrayList<Annonce> a = new ArrayList<Annonce>();
-		// JDBC objects
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		//requete de selection
+	// Method to insert an Annonce object into the database
+	public void insertAnnonce(Annonce annonce) throws SQLException {
+		String query = "INSERT INTO annonce (title, description, startDate, endDate, postsNum, secteur, fonction, " +
+				"experience, etudeLevel, contratDetails, url, siteName, adresseEntreprise, siteWebEntreprise, " +
+				"nomEntreprise, descriptionEntreprise, region, city, industry, traitsPersonnalite, competencesRequises, " +
+				"softSkills, competencesRecommandees, langue, niveauLangue, salaire, avantagesSociaux, teletravail) " +
+				"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+		try (PreparedStatement stmt = connection.prepareStatement(query)) {
+			stmt.setString(1, annonce.getTitle());
+			stmt.setString(2, annonce.getDescription());
+			stmt.setString(3, annonce.getStartDate());
+			stmt.setString(4, annonce.getEndDate());
+			stmt.setInt(5, annonce.getPostsNum());
+			stmt.setString(6, annonce.getSecteur());
+			stmt.setString(7, annonce.getFonction());
+			stmt.setString(8, annonce.getExperience());
+			stmt.setString(9, annonce.getEtudeLevel());
+			stmt.setString(10, annonce.getContratDetails());
+			stmt.setString(11, annonce.getUrl());
+			stmt.setString(12, annonce.getSiteName());
+			stmt.setString(13, annonce.getAdresseEntreprise());
+			stmt.setString(14, annonce.getSiteWebEntreprise());
+			stmt.setString(15, annonce.getNomEntreprise());
+			stmt.setString(16, annonce.getDescriptionEntreprise());
+			stmt.setString(17, annonce.getRegion());
+			stmt.setString(18, annonce.getCity());
+			stmt.setString(19, annonce.getIndustry());
+			stmt.setString(20, annonce.getTraitsPersonnalite());
+			stmt.setString(21, annonce.getCompetencesRequises());
+			stmt.setString(22, annonce.getSoftSkills());
+			stmt.setString(23, annonce.getCompetencesRecommandees());
+			stmt.setString(24, annonce.getLangue());
+			stmt.setString(25, annonce.getNiveauLangue());
+			stmt.setString(26, annonce.getSalaire());
+			stmt.setString(27, annonce.getAvantagesSociaux());
+			stmt.setString(28, annonce.getTeletravail());
+			stmt.executeUpdate();
+		}
+	}
+
+
+	// Method to retrieve all Annonce objects from the database
+	public List<Annonce> getAllAnnonces() throws SQLException {
 		String query = "SELECT * FROM annonce";
-		try {
-			//preparation a la connection
-			Class.forName("com.mysql.cj.jdbc.Driver");
+		List<Annonce> annonces = new ArrayList<>();
 
-			//Connection
-			connection = DriverManager.getConnection(url,user,password);
-			System.out.println("Connected to the database!");
-
-
-		}catch(Exception e){
-			System.out.println("Connection failed");
-		}
-		try {
-			preparedStatement = connection.prepareStatement(query);
-			// Executing the query
-			ResultSet resultSet = preparedStatement.executeQuery();
-			while(resultSet.next()){
-				Annonce test = new Annonce(resultSet.getInt(1),resultSet.getString(2),resultSet.getString(3),resultSet.getString(4),resultSet.getString(5)
-						,resultSet.getString(6),resultSet.getInt(7),resultSet.getString(8),resultSet.getString(9),resultSet.getString(10),resultSet.getString(11),
-						resultSet.getString(12));
-				a.add(test);
+		try (Statement stmt = connection.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
+			while (rs.next()) {
+				Annonce annonce = new Annonce(
+						rs.getInt("id"),
+						rs.getString("title"),
+						rs.getString("description"),
+						rs.getString("startDate"),
+						rs.getString("endDate"),
+						rs.getInt("postsNum"),
+						rs.getString("secteur"),
+						rs.getString("fonction"),
+						rs.getString("experience"),
+						rs.getString("etudeLevel"),
+						rs.getString("contratDetails"),
+						rs.getString("url"),
+						rs.getString("siteName"),
+						rs.getString("adresseEntreprise"),
+						rs.getString("siteWebEntreprise"),
+						rs.getString("nomEntreprise"),
+						rs.getString("descriptionEntreprise"),
+						rs.getString("region"),
+						rs.getString("city"),
+						rs.getString("industry"),
+						rs.getString("traitsPersonnalite"),
+						rs.getString("competencesRequises"),
+						rs.getString("softSkills"),
+						rs.getString("competencesRecommandees"),
+						rs.getString("langue"),
+						rs.getString("niveauLangue"),
+						rs.getString("salaire"),
+						rs.getString("avantagesSociaux"),
+						rs.getString("teletravail")
+				);
+				annonces.add(annonce);
 			}
 		}
-		catch(Exception e) {
-			System.out.println("Selection failed");
-		}
 
-		// Step 5: Close the resources
-		try {
-			if (preparedStatement != null) preparedStatement.close();
-			if (connection != null) connection.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return a;
-
+		return annonces;
 	}
 
+	// Method to update an existing Annonce in the database
+	public void updateAnnonce(Annonce annonce) throws SQLException {
+		String query = "UPDATE annonce SET title = ?, description = ?, startDate = ?, endDate = ?, postsNum = ?, " +
+				"secteur = ?, fonction = ?, experience = ?, etudeLevel = ?, contratDetails = ?, url = ?, siteName = ?," +
+				" adresseEntreprise = ?, siteWebEntreprise = ?, nomEntreprise = ?, descriptionEntreprise = ?, region = ?, " +
+				"city = ?, industry = ?, traitsPersonnalite = ?, competencesRequises = ?, softSkills = ?," +
+				" competencesRecommandees = ?, langue = ?, niveauLangue = ?, salaire = ?, avantagesSociaux = ?, " +
+				"teletravail = ? WHERE id = ?";
+
+		try (PreparedStatement stmt = connection.prepareStatement(query)) {
+			stmt.setString(1, annonce.getTitle());
+			stmt.setString(2, annonce.getDescription());
+			stmt.setString(3, annonce.getStartDate());
+			stmt.setString(4, annonce.getEndDate());
+			stmt.setInt(5, annonce.getPostsNum());
+			stmt.setString(6, annonce.getSecteur());
+			stmt.setString(7, annonce.getFonction());
+			stmt.setString(8, annonce.getExperience());
+			stmt.setString(9, annonce.getEtudeLevel());
+			stmt.setString(10, annonce.getContratDetails());
+			stmt.setString(11, annonce.getUrl());
+			stmt.setString(12, annonce.getSiteName());
+			stmt.setString(13, annonce.getAdresseEntreprise());
+			stmt.setString(14, annonce.getSiteWebEntreprise());
+			stmt.setString(15, annonce.getNomEntreprise());
+			stmt.setString(16, annonce.getDescriptionEntreprise());
+			stmt.setString(17, annonce.getRegion());
+			stmt.setString(18, annonce.getCity());
+			stmt.setString(19, annonce.getIndustry());
+			stmt.setString(20, annonce.getTraitsPersonnalite());
+			stmt.setString(21, annonce.getCompetencesRequises());
+			stmt.setString(22, annonce.getSoftSkills());
+			stmt.setString(23, annonce.getCompetencesRecommandees());
+			stmt.setString(24, annonce.getLangue());
+			stmt.setString(25, annonce.getNiveauLangue());
+			stmt.setString(26, annonce.getSalaire());
+			stmt.setString(27, annonce.getAvantagesSociaux());
+			stmt.setString(28, annonce.getTeletravail());
+			stmt.setInt(29, annonce.getId());
+			stmt.executeUpdate();
+		}
+	}
+
+	// Close the database connection
+	public void close() throws SQLException {
+		if (connection != null && !connection.isClosed()) {
+			connection.close();
+		}
+	}
 }
